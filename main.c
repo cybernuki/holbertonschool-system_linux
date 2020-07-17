ead79f219942a7d74030138662223af5ccd119c1
Cybernuki <1164@holbertonschool.com>
2020-07-17 13:19:03 +0000

    prints plane directories

--- main.c
+++ main.c
@@ -1,14 +1,29 @@
 #include "./headers/options.h"
 #include "./headers/parser.h"
+#include <sys/types.h>
+#include <dirent.h>
+
 
 int main (int argc, char *argv[]) {
 	paths *paths = NULL;
 	options *options = NULL;
+	DIR *dir = NULL;
+	struct dirent *read;
+	size_t i = 0;
+
 
 	paths = open_paths();
 	options = init_options();
 
 	parser(argc - 1, &argv[1], &paths, &options);
+
+
+	for (i = 0; i < paths->size; i++) {
+		dir = opendir(paths->list[i]);
+
+		while ((read = readdir(dir)) != NULL)
+			printf("%s\n", read->d_name);
+	}
 
 	close_paths(paths);
 	close_options(options);
