e5714562d062f18ce5bab3329ff03232ec810b65
Cybernuki <1164@holbertonschool.com>
2020-07-17 22:56:48 +0000

    comments in the data structure header

--- 0x00-ls/headers/data_structures.h
+++ 0x00-ls/headers/data_structures.h
@@ -8,7 +8,7 @@
 
 /**
  * struct dlistint_s - doubly linked list
- * @n: integer
+ * @value: dirent value
  * @prev: points to the previous node
  * @next: points to the next node
  *
@@ -23,13 +23,38 @@ typedef struct dlistint_s
 } to_print;
 
 
+/**
+ * add_node - adds a node in a double linked list at the end
+ * @head: head of the list
+ * @value: value of the node
+ * Return: a pointer to the new node
+ */
 to_print *add_node(to_print **head, struct dirent *value);
 
+/**
+ * remove_node - removes a node in a given position
+ * @head: head of the list
+ * @index: index of the new node
+ * Return: 1 if it succeeded, -1 if it failed
+ */
 int remove_node(to_print **head, unsigned int index);
 
+/**
+ * free_list - Free a double linked list
+ * @head: head of the list
+ * Return: nothing
+ */
 void free_list(to_print *head);
 
-void sort_list(to_print *list);
+/**
+ * sort_list - sort the linked list in place
+ * @head: is the head of the list
+ * @by: the criterio to be sorted, it could be 'S' to sort by size
+ * 't' to sort by time or, 'r' to reverse the list
+ * 
+ * retur
+ */
+void sort_list(to_print **head, char by, char order);
 
 
 void print_list(const to_print *h);
