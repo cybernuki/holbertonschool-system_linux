b453a19389147b6a103f620942c906a55f2982c0
--replace-all <1164@holbertonschool.com>
2020-09-24 03:13:44 +0000

    Part 6

---
+++ 0x06-signals/6-suspend.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * signal_handler - handles for a given signal
+ * @signal: is the given signal
+ */
+void signal_handler(int signal)
+{
+	printf("Caught %d\n", signal);
+}
+
+/**
+ * main - suspend the program until get SIGINT twice
+ * Return: EXIT_SUCESS
+ */
+int main(void)
+{
+	struct sigaction handler = {0};
+
+	handler.sa_handler = &signal_handler;
+	sigaction(SIGINT, &handler, NULL);
+
+	pause();
+	printf("Signal received\n");
+	return (EXIT_SUCCESS);
+}
