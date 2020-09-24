7a8ce3d758b1a3a5157f9790a6f2d7327bf5f31f
--replace-all <1164@holbertonschool.com>
2020-09-24 01:39:48 +0000

    Part 2

---
+++ 0x06-signals/2-handle_sigaction.c
@@ -0,0 +1,22 @@
+#include "signals.h"
+
+/**
+ * signal_handler - handles for a given signal
+ * @signal: is the given signal
+ */
+void signal_handler(int signal)
+{
+	printf("Gotcha! [%d]\n", signal);
+}
+
+/**
+ * handle_sigaction - set a handle function for the SIGINT signal
+ * Return: 0 on success, -1 in failure
+ */
+int handle_sigaction(void)
+{
+	struct sigaction handler = {0};
+
+	handler.sa_handler = &signal_handler;
+	return (sigaction(SIGINT, &handler, NULL));
+}
--- 0x06-signals/signals.h
+++ 0x06-signals/signals.h
@@ -8,5 +8,6 @@
 
 int handle_signal(void);
 void (*current_handler_signal(void))(int);
+int handle_sigaction(void);
 
 #endif
