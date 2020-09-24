0b7a47e96a21503ece1b76cca25b21fcb85bf3e4
--replace-all <1164@holbertonschool.com>
2020-09-24 02:30:59 +0000

    Part 4

---
+++ 0x06-signals/4-trace_signal_sender.c
@@ -0,0 +1,28 @@
+#include "signals.h"
+
+/**
+ * action - is the action for the SIGQUIT signal
+ * @signal: is the recieved signal
+ * @info: is a struct with information about the sender of the signal
+ * @context: is the context... whatever it is xddd
+ */
+void action(int signal, siginfo_t *info, void *context)
+{
+	(void)signal, (void)context;
+
+	printf("SIGQUIT sent by %d\n", info->si_pid);
+}
+
+/**
+ * trace_signal_sender - set handler for SIGQUIT signal
+ * Return: 0 in success, -1 in failure
+ */
+int trace_signal_sender(void)
+{
+	struct sigaction act = {0};
+
+	act.sa_sigaction = action;
+	act.sa_flags = SA_SIGINFO;
+
+	return (sigaction(SIGQUIT, &act, NULL));
+}
