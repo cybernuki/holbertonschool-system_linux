1f964863914c98f580332a9a48fa0026717d78c3
--replace-all <1164@holbertonschool.com>
2020-10-06 04:05:15 +0000

    part 4

--- 0x05-libasm/libasm.h
+++ 0x05-libasm/libasm.h
@@ -5,5 +5,6 @@ int asm_strlen(char *a);
 int asm_strcmp(const char *s1, const char *s2);
 int asm_strncmp(const char *s1, const char *s2, size_t n);
 char *asm_strchr(const char *s, int c);
+char *asm_strstr(const char *haystack, const char *needle);
 
 #endif
