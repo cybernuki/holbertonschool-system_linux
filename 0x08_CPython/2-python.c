3d8753551248ee87494014958dbd302206d97e66
cybernuki <1164@holbertonschool.com>
2020-12-09 03:19:15 +0000

    fix

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -13,7 +13,7 @@ void print_python_bytes(PyObject *p)
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
-		printf("    [ERROR] Invalid Bytes Object\n");
+		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
@@ -45,7 +45,7 @@ void print_python_list(PyObject *p)
 	{
 		element = ((PyListObject *)p)->ob_item[i];
 		printf("Element %zd: %s\n", i, (char *)(element->ob_type)->tp_name);
-		if (pobj->ob_type == &PyBytes_Type)
+		if (PyBytes_Check(element))
 			print_python_bytes(element);
 	}
 }
