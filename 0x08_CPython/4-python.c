42f268789266aa02720e79bdc1de7223d6a46841
cybernuki <1164@holbertonschool.com>
2020-12-09 04:02:39 +0000

    fix

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -10,7 +10,7 @@ void print_python_string(PyObject *p)
 {
 	printf("[.] string object info\n");
 
-	if (PyUnicode_Check(p))
+	if (!PyUnicode_Check(p))
 	{
 		printf("  [ERROR] Invalid String Object\n");
 		return;
