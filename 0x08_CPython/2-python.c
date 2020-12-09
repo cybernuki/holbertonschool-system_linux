096c31195434b19722f9a850ff7a6380f285df2f
cybernuki <1164@holbertonschool.com>
2020-12-09 03:17:07 +0000

    fix

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -45,5 +45,9 @@ void print_python_list(PyObject *p)
 	{
 		element = ((PyListObject *)p)->ob_item[i];
 		printf("Element %zd: %s\n", i, (char *)(element->ob_type)->tp_name);
+		if (PyBytes_Check(element))
+		{
+			print_python_bytes(element);
+		}
 	}
 }
