12d6906b7cb8adb0d551c881193262219fae6406
cybernuki <1164@holbertonschool.com>
2020-12-21 03:12:08 +0000

    fix betty

--- 0x0A-multithreading/0-thread_entry.c
+++ 0x0A-multithreading/0-thread_entry.c
@@ -1,5 +1,10 @@
 #include "multithreading.h"
 
+/**
+ * thread_entry - define an entry
+ * @arg: are the args
+ * Return: a void pointer
+ */
 void *thread_entry(void *arg)
 {
 	const char *string = (char *)arg;
