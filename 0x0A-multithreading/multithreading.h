1d3c5178f3d4ad1d0b13bc66be00d8441fc5337e
cybernuki <1164@holbertonschool.com>
2020-12-23 05:00:34 +0000

    fix

--- 0x0A-multithreading/multithreading.h
+++ 0x0A-multithreading/multithreading.h
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include "list.h"
+#include "pool_threading.h"
 
 /**
  * struct pixel_s - RGB pixel
@@ -78,5 +79,9 @@ void blur_image(img_t *img_blur, img_t const *img, kernel_t const *kernel);
 int tprintf(char const *format, ...);
 
 list_t *prime_factors(char const *s);
+
+task_t *create_task(task_entry_t entry, void *param);
+void destroy_task(task_t *task);
+void *exec_tasks(list_t const *tasks);
 
 #endif
