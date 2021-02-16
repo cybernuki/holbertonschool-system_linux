c95eede83d995b8627e199c7e9399e559d741709
cybernuki <1164@holbertonschool.com>
2021-02-16 01:10:13 +0000

    lint

--- 0x0C-sockets/0-server.c
+++ 0x0C-sockets/0-server.c
@@ -11,31 +11,31 @@
  */
 int main()
 {
-    int sd;
-    struct sockaddr_in name;
+	int sd;
+	struct sockaddr_in name;
 
-    sd = socket(PF_INET, SOCK_STREAM, 0);
-    if (sd < 0)
-    {
-        perror("socket failed");
-        return (EXIT_FAILURE);
-    }
-    name.sin_family = AF_INET;
-    name.sin_port = htons(PORT);
-    name.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (bind(sd, (struct sockaddr *)&name, sizeof(name)) < 0)
-    {
-        perror("bind failure");
-        return (EXIT_FAILURE);
-    }
-    if (listen(sd, 10) < 0)
-    {
-        perror("listen failure");
-        return (EXIT_FAILURE);
-    }
+	sd = socket(PF_INET, SOCK_STREAM, 0);
+	if (sd < 0)
+	{
+		perror("socket failed");
+		return (EXIT_FAILURE);
+	}
+	name.sin_family = AF_INET;
+	name.sin_port = htons(PORT);
+	name.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (bind(sd, (struct sockaddr *)&name, sizeof(name)) < 0)
+	{
+		perror("bind failure");
+		return (EXIT_FAILURE);
+	}
+	if (listen(sd, 10) < 0)
+	{
+		perror("listen failure");
+		return (EXIT_FAILURE);
+	}
 
-    printf("Server listening on port 12345\n");
-    while (1)
-        ;
-    return (EXIT_SUCCESS);
+	printf("Server listening on port 12345\n");
+	while (1)
+		;
+	return (EXIT_SUCCESS);
 }
